13a892a3f8f1ccab9260c6e26fa964423005f92d
Gabriel Oliveira <gabriel_oliveira_f_sousa@outlook.com>
2020-03-24 13:14:14 -0300

    Gerenciamento de Memoria Value e reference-Types

--- src/estruturadados/EstruturaDados.java
+++ src/estruturadados/EstruturaDados.java
@@ -1,5 +1,43 @@
 package estruturadados;
 
+import java.util.Scanner;
+import modelos.Pessoa;
+
 public class EstruturaDados {
 
+    public static void main(String[] args) {
+        System.out.println("Digite opção desejkada");
+        System.out.println("1. Gerenciamento de memoria: ");
+        Scanner scanf = new Scanner(System.in);
+        int opcao = scanf.nextInt();
+        
+        switch (opcao) {
+            case 1:
+                fazerGerenciamentoMemoria();    
+                break;
+        }
+        scanf.close();
+
+    }
+
+    private static void fazerGerenciamentoMemoria() {
+        int a = 3;  
+        System.out.println(a);
+        int b = a;
+        System.out.println(b);
+        b = 2;
+        System.out.println("--------");
+        System.out.println(b);
+        System.out.println(a == b);
+        
+        System.out.println("=========");
+        Pessoa p1 = new Pessoa(1,"TreinaWeb");
+        System.out.println(p1.toString());
+        Pessoa p2 = p1;
+        System.out.println(p2.toString());
+        p2.setNome("TreinaWeb Modificado");
+        System.out.println(p1.toString());
+        System.out.println(p2.toString());
+        System.out.println(p1 == p2);
+    }
 }
---
+++ src/modelos/Pessoa.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelos;
+
+/**
+ *
+ * @author gabri
+ */
+public class Pessoa {
+
+    private int id;
+    private String nome;
+
+    public Pessoa(int id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" + "id=" + id + ", nome=" + nome + '}';
+    }
+
+}
