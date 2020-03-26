7658118d174c9c6a5439edb21b7625c9d5a566f4
Gabriel Oliveira <gabriel_oliveira_f_sousa@outlook.com>
2020-03-26 15:16:48 -0300

    Criando funcionalidade de insercao e recuperacao

--- src/estruturadadolistaligada/ListaLigada.java
+++ src/estruturadadolistaligada/ListaLigada.java
@@ -22,6 +22,52 @@ public class ListaLigada<T> {
         this.tamanho = 0;
     }
 
+    public void inserirEn(int posicao, T elemento) {
+        if (posicao >= tamanho()) {
+            throw new IllegalArgumentException(String.format("Posicao invalida [%d]", posicao));
+        }
+        if (posicao == 0) {
+            No<T> novoNo = new No<T>(elemento);
+            novoNo.setProximo(this.primeiroNo);
+            this.primeiroNo = novoNo;
+        } else if (posicao == this.tamanho()) {
+            No<T> novoNo = new No<T>(elemento);
+            this.ultimoNo.setProximo(novoNo);
+            this.ultimoNo = novoNo;
+        } else {
+
+            No<T> noAnterior = recuperarNo(posicao - 1);
+            No<T> noAtual = recuperarNo(posicao);
+            No<T> novoNo = new No<>(elemento);
+            novoNo.setProximo(noAtual);
+
+        }
+        this.tamanho++; 
+    }
+
+    private No<T> recuperarNo(int posicao) {
+        if (posicao >= tamanho()) {
+            throw new IllegalArgumentException(String.format("Posicao invalida [%d]", posicao));
+        }
+        No<T> resultado = null;
+        for (int i = 0; i <= posicao; i++) {
+            if (i == 0) {
+                resultado = this.primeiroNo;
+            } else {
+                resultado = resultado.getProximo();
+            }
+        }
+        return resultado;
+    }
+
+    public T recuperar(int posicao) {
+        No<T> no = recuperarNo(posicao);
+        if (no != null) {
+            return no.getElemento();
+        }
+        return null;
+    }
+
     public void inserir(T elemento) {
         No<T> novoNo = new No<>(elemento);
         if (estaVazia()) {
